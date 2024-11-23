85163fd74c7f6b597d5745e4cec056ef7aa51213
Avirup Ghosh <ag.avirup1997@gmail.com>
2024-11-23 08:24:07 +0530

    🚧 updated packages

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-jsdoc', 'prettier-plugin-pkg', 'prettier-plugin-tailwindcss'],
+  printWidth: 120,
+  quoteProps: 'consistent',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
